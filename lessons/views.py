cb7312eea35756935c8b4cc7f6909f18ed9bc2eb
BaktilekTaalaybekov <taalaibekovbaktilek@gmail.com>
2022-01-12 16:02:11 +0600

    add category.html

--- lessons/views.py
+++ lessons/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
-from .models import Lesson
+from .models import Lesson, Category
 
 
 def index(request):
     lessons = Lesson.objects.all()
+    categories = Category.objects.all()
     context = {
         'lessons_k': lessons,
-        'title_k': 'LESSONS LIST'
+        'title_k': 'LESSONS LIST',
+        'categories_k': categories,
     }
     return render(request, 'lessons/index.html', context)
+
+
+def get_category(request, category_id):
+    lessons = Lesson.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'lessons/category.html',
+                  {'lessons_k_gk': lessons, 'categories_k_gk': categories, 'category_k_gk': category})

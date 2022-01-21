e1a762bc4b5831bb0166649473b33fa1035357cf
@taalaibekovbaktilek <taalaibekovbaktilek@gmail.com>
2022-01-21 19:52:36 +0600

    add tags_2 and some changes

--- lessons/templatetags/lesson_tags.py
+++ lessons/templatetags/lesson_tags.py
@@ -5,6 +5,12 @@ from lessons.models import Category
 register = template.Library()
 
 
-@register.simple_tag()
+@register.simple_tag(name='get_list_categories')
 def get_categories():
     return Category.objects.all()
+
+
+@register.inclusion_tag('lessons/list_categories.html')
+def show_categories():
+    categories = Category.objects.all()
+    return {"categories_k": categories}
--- lessons/views.py
+++ lessons/views.py
@@ -1,21 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Lesson, Category
 
 
 def index(request):
     lessons = Lesson.objects.all()
-    categories = Category.objects.all()
     context = {
         'lessons_k': lessons,
         'title_k': 'LESSONS LIST',
-        'categories_k': categories,
     }
     return render(request, 'lessons/index.html', context)
 
 
 def get_category(request, category_id):
     lessons = Lesson.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
     return render(request, 'lessons/category.html',
-                  {'lessons_k_gk': lessons, 'categories_k_gk': categories, 'category_k_gk': category})
+                  {'lessons_k_gk': lessons, 'category_k_gk': category})
+
+
+def view_lesson(request, lesson_id):
+    # lesson_item = Lesson.objects.get(pk=lesson_id)
+    lesson_item = get_object_or_404(Lesson, pk=lesson_id)
+    return render(request, 'lessons/view_lesson.html', {"lesson_item": lesson_item})
